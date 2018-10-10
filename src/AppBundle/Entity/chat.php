aab9348ddd19650c102d3c3d7048d9d2c35af6dc
alexdhorne <alexdhorne69210@gmail.com>
2018-10-10 10:25:59 +0200

    ajout de toutes les entités, création de la base de donnée

---
+++ src/AppBundle/Entity/chat.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * chat
+ *
+ * @ORM\Table(name="chat")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\chatRepository")
+ */
+class chat
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return chat
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
+
